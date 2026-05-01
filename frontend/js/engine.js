fd716afaf36929c5efe0cfad767d021ee73b22cf
Karl0259 <karldiril6@gmail.com>
2026-05-01 22:34:34 +0200

    feat: adding delete feature

--- frontend/js/engine.js
+++ frontend/js/engine.js
@@ -4,7 +4,7 @@ let i = 0;
 jeu[i].classList.add("actif");
 
 document.body.addEventListener("keydown", (input) => {
-    console.log("test");
+    console.log(input.key);
     if (input.key.length == 1) {
         if (input.key == jeu[i].textContent) {
             jeu[i].classList.add("correct");
@@ -12,13 +12,24 @@ document.body.addEventListener("keydown", (input) => {
         else {
             jeu[i].classList.add("incorrect");
         }
+
         jeu[i].classList.remove("actif");
+
         if (i < jeu.length - 1) {  // Vérifier si on arrive à la dernière lettre
             ++i;
             jeu[i].classList.add("actif");
         }
         else {
             console.log("fini");
+        }
+    }
+    else if (input.key == "Backspace") {
+        if (i > 0) {
+            jeu[i].classList.remove("actif");
+            --i;
+            jeu[i].classList.add("actif");
+            jeu[i].classList.remove("correct");
+            jeu[i].classList.remove("incorrect");
         }
     }
 })
