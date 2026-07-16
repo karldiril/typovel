58a268470ab513c50d117dc1c133c474d568e57b
Karl0259 <karldiril6@gmail.com>
2026-07-16 13:01:12 +0200

    feat: implement rendering offset and absolute word identifiers

--- frontend/js/gameUI.js
+++ frontend/js/gameUI.js
@@ -2,18 +2,31 @@ import { Engine } from './engine.js';
 
 
 const game = document.querySelector(".jeu");
+let offset = 0;
+
+
 
 export function updateUI(engine) {
     game.innerHTML = "";
     let finalUI = "";
-    for (const objet of engine.tabMots) {
-        finalUI += `<div class="word">`;
 
-        const lettres = objet.lettres;
+    const motsAafficher = engine.tabMots.slice(offset, offset + 100);
 
-        for (let i = 0; i < lettres.length; i++) {
-            let etat = lettres[i].charEtat;
-            let content = lettres[i].charAttendu;
+
+    for (let i = 0; i < motsAafficher.length; i++) {
+        
+        const wordObject = motsAafficher[i];
+        const indexAbsolu = offset + i;
+
+        finalUI += `<div class="word" data-wordindex="${indexAbsolu}">`;
+
+        console.log(wordObject);
+
+        const lettres = wordObject.lettres;
+
+        for (let j = 0; j < lettres.length; j++) {
+            let etat = lettres[j].charEtat;
+            let content = lettres[j].charAttendu;
             switch (etat) {
                 case "CORRECT":
                     finalUI += `<span class="correct">${content}</span>`;
@@ -25,7 +38,7 @@ export function updateUI(engine) {
                     finalUI += `<span>${content}</span>`;
                     break;
                 case "EXTRA":
-                    finalUI += `<span class="incorrect extra">${lettres[i].charTape}</span>`;
+                    finalUI += `<span class="incorrect extra">${lettres[j].charTape}</span>`;
                     break;
             }
                 
@@ -40,9 +53,10 @@ export function updateUI(engine) {
 
 
 export function curseurPlacement(engine) {
-    const currentWord = document.querySelectorAll(".word")[engine.currentWordIndex];
-    const currentLetter = currentWord.children[engine.currentLetterIndex + 1];
-    console.log(engine.currentLetterIndex);
+    const currentWord = document.querySelector(`.word[data-wordindex="${engine.currentWordIndex}"]`);
+    
+    if (!currentWord) return;
+
     const caret = document.querySelector(".caret");
 
 
