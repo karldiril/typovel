513bf39903bed26e1f19abe631c2cfb88b36f21f
Karl0259 <karldiril6@gmail.com>
2026-09-04 15:39:24 +0200

    feat: add RegisterDto and LoginDto

---
+++ backend/DTOs/RegisterDto.cs
@@ -0,0 +1,10 @@
+namespace backend.DTOs;
+
+
+
+public class RegisterDto
+{
+    public string Pseudo { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
